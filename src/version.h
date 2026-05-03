da97a70cfcd2c0dc2bb3a2225e1c71be74689a03
Revenberg <sander@revenberg.info>
2026-05-03 10:49:00 +0200

    Bump web version and update index

--- src/version.h
+++ src/version.h
@@ -3,10 +3,16 @@
 // Auto-incremented version number
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 0
 
 // Build number (auto-incremented on each deploy)
 #define BUILD_NUMBER 0
 
+// Patch version follows build number
+#define VERSION_PATCH BUILD_NUMBER
+
+// Stringify helpers for macro values
+#define VERSION_STR_HELPER(x) #x
+#define VERSION_STR(x) VERSION_STR_HELPER(x)
+
 // Version string
-#define VERSION_STRING "v1.0.0"
+#define VERSION_STRING "v" VERSION_STR(VERSION_MAJOR) "." VERSION_STR(VERSION_MINOR) "." VERSION_STR(VERSION_PATCH)
